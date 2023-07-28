daf3035cacc83a5f37176f59ea58a74c4c824083
FonDitbul <niki7084@naver.com>
2023-07-28 21:08:15 +0900

    chore : product 수정

--- src/product/infrastructure/product.prisma.repository.ts
+++ src/product/infrastructure/product.prisma.repository.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../database/infrastructure/prisma.service';
-import { ProductWhere } from '../domain/product.repository';
+import { IProductRepository, ProductWhere } from '../domain/product.repository';
 import { removeUndefinedKey } from '../../util/json.util';
 
 @Injectable()
-export class ProductPrismaRepository {
+export class ProductPrismaRepository implements IProductRepository {
   constructor(private prisma: PrismaService) {}
   async findOne(where: ProductWhere) {
     const whereCondition = removeUndefinedKey({

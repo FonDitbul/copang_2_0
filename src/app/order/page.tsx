5ad781f04fde8611239a80374d70681e39d1efb2
FonDitbul <niki7084@naver.com>
2023-08-22 23:12:18 +0900

    add : cart, order design 적용

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,7 +1,14 @@
+import OrderCard from "@/components/Order/OrderCard";
+
 export default function Order() {
   return (
     <div>
-      <p>주문 내역 보기</p>
+      <h1>구매 내역 보기</h1>
+      <OrderCard/>
+      <OrderCard/>
+      <OrderCard/>
+      <OrderCard/>
+      <OrderCard/>
     </div>
   )
 }
--- src/components/account/login.tsx
+++ src/components/account/login.tsx
@@ -5,9 +5,6 @@ export default function AccountLogin() {
         <div
           className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
-            </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> 아이디 </label>
@@ -34,7 +31,7 @@ export default function AccountLogin() {
                 <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">비밀번호를 잊어버리셨나요?</a>
               </div>
               <button type="submit"
-                      className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">로그인
+                      className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">로그인
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">계정 생성하기</a>

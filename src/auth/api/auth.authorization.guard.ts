19dc1c34c28d7033d47dd67d5fb870f75d638f1e
FonDitbul <niki7084@naver.com>
2023-08-23 21:19:27 +0900

    chore : bearerToken 이 아예 존재하지 않을 시 예외처리 변경

--- src/auth/api/auth.authorization.guard.ts
+++ src/auth/api/auth.authorization.guard.ts
@@ -1,5 +1,6 @@
 import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
 import { ILoginToken } from '../domain/login.token';
+import { CoPangException, EXCEPTION_STATUS } from '../../common/domain/exception';
 
 @Injectable()
 export class AuthAuthorizationGuard implements CanActivate {
@@ -8,8 +9,12 @@ export class AuthAuthorizationGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const ctx = context.switchToHttp();
     const request = ctx.getRequest();
+    const bearerToken = request.get('Authorization');
+    if (!bearerToken) {
+      throw new CoPangException(EXCEPTION_STATUS.LOGIN_TOKEN_ERROR);
+    }
 
-    request.user = this.loginToken.verifyByAccess(request.get('Authorization').split(' ')[1] ?? '');
+    request.user = this.loginToken.verifyByAccess(bearerToken.split(' ')[1]);
 
     return true;
   }

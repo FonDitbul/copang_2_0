a62efb4ef11e4ab6fb87bb4f7d5ce31142981f3d
FonDitbul <niki7084@naver.com>
2023-08-31 15:29:50 +0900

    chore: cors 설정 port 변경

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { TransformInterceptor } from './common/api/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();

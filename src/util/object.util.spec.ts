caee84a88517c928bf4e7db0c1a9f11b0d535c86
FonDitbul <niki7084@naver.com>
2023-09-14 17:27:17 +0900

    add: object object key 체크 함수 추가

--- src/util/object.util.spec.ts
+++ src/util/object.util.spec.ts
@@ -1,4 +1,4 @@
-import { isEmptyObject } from './object.util';
+import { isContainKeyInObject, isEmptyObject, isNotContainKeyInObject } from './object.util';
 
 describe('object util test', () => {
   describe('isEmptyObject 함수 테스트', () => {
@@ -14,6 +14,46 @@ describe('object util test', () => {
       const givenObject = { test: true };
 
       const result = isEmptyObject(givenObject);
+
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe('isContainKeyInObject 함수 테스트', () => {
+    it('object 안에 해당 key 값의 데이터가 존재하여 true를 반환 하는 경우', () => {
+      const givenObject = {
+        test: 'test',
+      };
+
+      const result = isContainKeyInObject(givenObject, 'test');
+
+      expect(result).toBeTruthy();
+    });
+
+    it('object 안에 해당 key 값의 데이터가 존재하지 않아 false를 반환하는 경우', () => {
+      const givenObject = { testNotihing: true };
+
+      const result = isContainKeyInObject(givenObject, 'test');
+
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe('isNotContainKeyInObject 함수 테스트', () => {
+    it('object 안에 해당 key 값의 데이터가 존재하지않아 true를 반환 하는 경우', () => {
+      const givenObject = {
+        testNotContain: 'test',
+      };
+
+      const result = isNotContainKeyInObject(givenObject, 'test');
+
+      expect(result).toBeTruthy();
+    });
+
+    it('object 안에 해당 key 값의 데이터가 존재하여 false를 반환하는 경우', () => {
+      const givenObject = { test: true };
+
+      const result = isNotContainKeyInObject(givenObject, 'test');
 
       expect(result).toBeFalsy();
     });

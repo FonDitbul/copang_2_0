1d343be4567c2f5bfc57cf91f73d86f031921030
FonDitbul <niki7084@naver.com>
2023-09-24 16:39:58 +0900

    add : 주소 추가 대표 설정, 삭제 추가

--- src/components/Account/Address/AddressCard.Mole.tsx
+++ src/components/Account/Address/AddressCard.Mole.tsx
@@ -1,16 +1,38 @@
 import StarAtom from "../../Common/Atom/Star";
 import { BuyerAddress } from "../../../interface/BuyerAddress";
 import { Address } from "../../../interface/Address";
+import CloseButton from "../../Common/Atom/CloseButton";
+import React from "react";
+import { Client } from "../../../context/api";
+import AddressRepresentativeStar from "./AddressRepresentativeStar.Mole";
 
-export function AddressCardMole({ address }: BuyerAddress) {
-  const addressParse = JSON.parse(address) as Address;
+export function AddressCardMole(buyerAddress: BuyerAddress) {
+  const addressParse = JSON.parse(buyerAddress.address) as Address;
+
+  let star = <StarAtom isFull={buyerAddress.isRepresentative} />;
+  if (!buyerAddress.isRepresentative) {
+    // 대표 address가 아닌 경우
+    star = <AddressRepresentativeStar id={buyerAddress.id} />;
+  }
+
+  const deleteClickEvent = (e: React.MouseEvent<HTMLButtonElement>) => {
+    Client.delete("/buyer/address", {
+      data: {
+        id: buyerAddress.id,
+      },
+    }).then(() => {
+      location.reload();
+    });
+    return;
+  };
 
   return (
     <div className="p-5 border">
-      <StarAtom isFull={true} />
-      <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-        <a href="#">수신자</a>
-      </h3>
+      {star}
+      <CloseButton onClick={deleteClickEvent} />
+      {/*<h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">*/}
+      {/*  <a href="#">수신자</a>*/}
+      {/*</h3>*/}
       <div className="flex flex-col">
         <span className="text-gray-500 dark:text-gray-400"> {addressParse.postalCode} </span>
         <span className="text-gray-500 dark:text-gray-400"> {addressParse.roadAddress} </span>

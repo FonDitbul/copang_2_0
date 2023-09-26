d1f6fcf24abcb775db0432e13988c46c8a968e1d
FonDitbul <niki7084@naver.com>
2023-09-26 15:38:51 +0900

    chore: address 변경

--- src/buyer/api/buyerAccount.req.dto.ts
+++ src/buyer/api/buyerAccount.req.dto.ts
@@ -1,4 +1,4 @@
-import { IsIn, IsNotEmpty, IsNotEmptyObject, IsNumberString, IsOptional, Matches, MaxLength, Min, ValidateNested } from 'class-validator';
+import { IsNotEmpty, IsNotEmptyObject, IsNumberString, IsOptional, Matches, MaxLength, Min, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
 class CreateAddressReq {
@@ -7,13 +7,10 @@ class CreateAddressReq {
   postalCode: string; // 우편 번호 ex) 13561
 
   @IsNotEmpty()
-  addressRegion: string; // 주소 중 가장 큰 지역명 ex) 경기
+  address: string; // 전체 주소
 
   @IsNotEmpty()
-  addressLocality: string; // 하위 지역 ex) 성남시 분당구
-
-  @IsNotEmpty()
-  streetAddress: string; // 도로명 포함 상세 주소 ex) 정자일로 95
+  roadAddress: string; // 도로명 주소
 
   @IsOptional()
   etc?: string; // 기타 주소
--- src/buyer/domain/address.ts
+++ src/buyer/domain/address.ts
@@ -1,7 +1,6 @@
 export interface Address {
   postalCode: string; // 우편 번호 ex) 13561
-  addressRegion: string; // 주소 중 가장 큰 지역명 ex) 경기
-  addressLocality: string; // 하위 지역 ex) 성남시 분당구
-  streetAddress: string; // 도로명 포함 상세 주소 ex) 정자일로 95
+  address: string; // 전체 주소
+  roadAddress: string; // 도로명 주소
   etc?: string; // 기타 주소
 }

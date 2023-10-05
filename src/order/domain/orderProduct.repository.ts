f90ab8192d61f9af6ff8812b8da2ea1223a3b668
FonDitbul <niki7084@naver.com>
2023-10-05 22:39:39 +0900

    COB-26 : 구매 물품 주문 내역 불러오기 API 추가

--- src/order/domain/orderProduct.repository.ts
+++ src/order/domain/orderProduct.repository.ts
@@ -1,6 +1,8 @@
 import { OrderProduct } from './orderProduct';
+import { OrderProductFindAllOut } from './port/orderProduct.out';
 
 export interface IOrderProductRepository {
   findOneById(id: number): Promise<OrderProduct>;
   updateByReview(id: number, reviewId: number): Promise<OrderProduct>;
+  findAllByBuyerIdNoOffset(findAllOut: OrderProductFindAllOut): Promise<OrderProduct[]>;
 }

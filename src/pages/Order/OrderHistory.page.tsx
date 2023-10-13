43bf3865cf7246f02fff22f6e99bd4bb59f5b74d
FonDitbul <niki7084@naver.com>
2023-10-13 21:55:55 +0900

    chore : order 더보기 기능 추가

--- src/pages/Order/OrderHistory.page.tsx
+++ src/pages/Order/OrderHistory.page.tsx
@@ -1,5 +1,5 @@
 import OrderProductCard from "../../components/Order/OrderProductCard.Mole";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Client, ResponseData } from "../../context/api";
 import { OrderProduct } from "../../interface/OrderProduct";
 import Button from "../../components/Common/Atom/Button";
@@ -10,15 +10,24 @@ interface OrderProductResponse {
 
 export default function OrderHistoryPage() {
   const [orderProductArray, setOrderProductArray] = useState([] as OrderProduct[]);
+  const [orderLastId, setOrderLastId] = useState(0);
+
+  const getOrderProduct = async (lastId: number) => {
+    const response = await Client.get(`/buyer/order/product?lastId=${lastId}`);
+    const responseData = response.data as ResponseData<OrderProductResponse>;
+    const orderProducts = responseData.content.orderProducts;
+
+    return orderProducts;
+  };
 
   useEffect(() => {
-    const getOrderProduct = async () => {
-      const response = await Client.get("/buyer/order/product");
-      const responseData = response.data as ResponseData<OrderProductResponse>;
-      const orderProducts = responseData.content.orderProducts;
-      setOrderProductArray(orderProducts);
-    };
-    getOrderProduct();
+    getOrderProduct(0).then((result) => {
+      setOrderProductArray([...orderProductArray, ...result]);
+      const orderLast = result.at(-1);
+      if (orderLast) {
+        setOrderLastId(orderLast.id);
+      }
+    });
   }, []);
 
   if (orderProductArray.length === 0) {
@@ -28,6 +37,22 @@ export default function OrderHistoryPage() {
       </div>
     );
   }
+
+  const moreButtonEventHandler = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("?");
+    const orderLast = orderProductArray.at(-1);
+    if (orderLast && orderLast.id === orderLastId) {
+      const result = await getOrderProduct(orderLastId);
+      setOrderProductArray([...orderProductArray, ...result]);
+
+      const orderLast = result.at(-1);
+      if (orderLast) {
+        setOrderLastId(orderLast.id);
+      }
+    }
+    return;
+  };
+
   return (
     <div className="items-center place-content-center">
       <h1 className="mb-10 text-center text-2xl font-bold">구매 내역 보기</h1>
@@ -40,7 +65,7 @@ export default function OrderHistoryPage() {
           ))}
         </ul>
       </div>
-      <Button> 더 보기 </Button>
+      <Button onClick={moreButtonEventHandler}> 더 보기 </Button>
     </div>
   );
 }

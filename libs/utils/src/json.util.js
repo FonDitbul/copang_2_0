6a0a6a58c4fdb0d69b8b2f54cad21233583c1748
정권기 <49264688+FonDitbul@users.noreply.github.com>
2023-11-02 17:12:44 +0900

    [COMONO-2] batch 프로젝트 생성  (#24)
    * [COMONO-2] add batch Project
    
    * chore: ignore and lib add

---
+++ libs/utils/src/json.util.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.removeUndefinedKey = void 0;
+function removeUndefinedKey(value) {
+    return JSON.parse(JSON.stringify(value));
+}
+exports.removeUndefinedKey = removeUndefinedKey;
+//# sourceMappingURL=json.util.js.map

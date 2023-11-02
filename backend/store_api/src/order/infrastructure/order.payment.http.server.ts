ee7ea80ce96aa2122536287b412284ffbafb53ce
FonDitbul <niki7084@naver.com>
2023-11-02 23:19:48 +0900

    [COMONO-3] order 모듈 변경

--- backend/store_api/src/order/infrastructure/order.payment.http.server.ts
+++ backend/store_api/src/order/infrastructure/order.payment.http.server.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { IOrderPaymentServer } from '../domain/order.payment.server';
-import * as crypto from 'crypto';
 import { OrderPaymentRequestOut } from '../domain/port/order.out';
 import { OrderPaymentRequestIn } from '../domain/port/order.in';
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class OrderPaymentHttpServer implements IOrderPaymentServer {
+  constructor(private readonly httpService: HttpService) {}
   async request(requestOut: OrderPaymentRequestOut): Promise<OrderPaymentRequestIn> {
-    const paymentKeyMocking = crypto.randomUUID();
-    return {
-      paymentKey: paymentKeyMocking,
-    };
+    const { data } = await firstValueFrom(
+      this.httpService.post<{ paymentKey: string }>('http://localhost:5001/payment', requestOut).pipe(
+        catchError((error: AxiosError) => {
+          throw `${error} An error happened!`;
+        }),
+      ),
+    );
+    return data;
   }
 }

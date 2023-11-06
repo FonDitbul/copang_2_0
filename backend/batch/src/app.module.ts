8aea6733ea9fd524d8f48017fd9418bf3db0feed
FonDitbul <niki7084@naver.com>
2023-11-06 21:18:55 +0900

    [batch-2] product category crawler

--- backend/batch/src/app.module.ts
+++ backend/batch/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ShippingModule } from './shipping/shipping.module';
+import { ProductCrawlerModule } from './productCrawler/product.crawler.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), ShippingModule],
+  imports: [ScheduleModule.forRoot(), ShippingModule, ProductCrawlerModule],
   controllers: [],
   providers: [],
 })

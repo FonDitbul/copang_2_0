639d0b657f7be52d0f42db725ba259cc3104f95e
FonDitbul <niki7084@naver.com>
2023-11-08 18:30:13 +0900

    [Event] cart buy event 추가

--- backend/store_api/src/cart/application/cart.buy.event.handler.ts
+++ backend/store_api/src/cart/application/cart.buy.event.handler.ts
@@ -1,9 +1,14 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { CartBuyEvent } from '../domain/cart.buy.event';
+import { Inject } from '@nestjs/common';
+import { ICartRepository } from '../domain/cart.repository';
 
 @EventsHandler(CartBuyEvent)
 export class CartBuyEventHandler implements IEventHandler<CartBuyEvent> {
+  constructor(@Inject('ICartRepository') private cartRepository: ICartRepository) {}
   async handle(event: CartBuyEvent) {
-    console.log(event);
+    if (event.productIdArray.length === 0) return;
+
+    await this.cartRepository.deleteByBuy({ ...event });
   }
 }

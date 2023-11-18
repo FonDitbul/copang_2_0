8d2598cfb02be6f046d08754eb9d03d0768a32a9
FonDitbul <niki7084@naver.com>
2023-11-18 20:45:37 +0900

    [Swagger] add: swagger setting

--- backend/store_api/src/main.ts
+++ backend/store_api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { UseInterceptors, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/api/http-exception.filter';
 import { TransformInterceptor } from './common/api/transform.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('copang store api')
+    .setDescription('copang 프로젝트 stroe api 입니다.')
+    .setVersion('2.0')
+    .addTag('coapng')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(5000);
 }
 bootstrap();

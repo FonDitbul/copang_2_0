50c67dfb7b13413d96777401e29e31a0f51a878c
FonDitbul <niki7084@naver.com>
2023-11-30 19:20:51 +0900

    add : payment health add

--- backend/payment_api/src/app.module.ts
+++ backend/payment_api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentModule } from './payment/payment.module';
+import { HealthController } from './common/health.controller';
 
 @Module({
   imports: [PaymentModule],
-  controllers: [],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
